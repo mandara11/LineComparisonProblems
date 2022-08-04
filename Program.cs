740fda08a55d62d5118ad43251d7ffd5519a56b2
mandara11 <mandaram2345@gmail.com>
2022-08-04 23:39:05 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisonProblem2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welome To Line Comparision Problem");
+        }
+    }
+}
