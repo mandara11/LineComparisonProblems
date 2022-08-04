177e1e00af60d5154c7182387f7d45a3e1bb8e17
mandara11 <mandaram2345@gmail.com>
2022-08-04 23:54:33 +0530

    Added:UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,23 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welome To Line Comparision Problem");
+            int x1, x2, y1, y2;
+
+            Console.WriteLine("Welcome to SINGLE LINE LENGTH PROGRAM");
+
+
+            Console.WriteLine("Enter the coordinates value for points ");
+            Console.WriteLine("Enter coordinate x for point1");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter coordinate y for point1");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter coordinate x for point2");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter coordinate y for point2");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            Length obj1 = new Length();
+            obj1.LineLength(x1, x2, y1, y2);
         }
     }
 }
