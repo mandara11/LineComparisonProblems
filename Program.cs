cc4ea6f34cf5aed9734ea484561c3a17a3a5a10a
mandara11 <mandaram2345@gmail.com>
2022-08-05 00:01:29 +0530

    Added:UC2

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welome To Line Comparision Problem");
-            int x1, x2, y1, y2;
+            int x1, x2, x3, x4, y1, y2, y3, y4;
 
             Console.WriteLine("Welcome to SINGLE LINE LENGTH PROGRAM");
 
@@ -20,8 +20,19 @@
             Console.WriteLine("Enter coordinate y for point2");
             y2 = Convert.ToInt32(Console.ReadLine());
 
-            Length obj1 = new Length();
-            obj1.LineLength(x1, x2, y1, y2);
+            Console.WriteLine("Enter coordinate x for point1 of Line2");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter coordinate y for point1 of Line2");
+            y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter coordinate x for point2 of Line2");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter coordinate y for point2 of Line2");
+            y4 = Convert.ToInt32(Console.ReadLine());
+            TwoLineCompare obj2 = new TwoLineCompare();
+            obj2.TwoLineLength(x1, x2, x3, x4, y1, y2, y3, y4);
+
+            //Length obj1 = new Length(); //UC1
+            //obj1.LineLength(x1, x2, y1, y2);
         }
     }
 }
